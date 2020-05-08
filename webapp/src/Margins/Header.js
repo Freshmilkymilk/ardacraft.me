64e3123f488a20bcf18532937e41b5a32dd04cae
Michael Anderson <yadoneson175@gmail.com>
2020-05-07 23:46:06 -0600

    Add nav

--- webapp/src/Margins/Header.js
+++ webapp/src/Margins/Header.js
@@ -1,63 +1,198 @@
 import React from 'react';
+import clsx from 'clsx';
+
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
+import {fade, makeStyles} from '@material-ui/core/styles';
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
+
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
-import MoreIcon from '@material-ui/icons/MoreVert';
 
-const myStyles = makeStyles((theme) => ({
+const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
     },
     menuButton: {
         marginRight: theme.spacing(2),
-        color: 'white',
-    },
-    toolbar: {
-        minHeight: 128,
-        alignItems: 'flex-start',
-        paddingTop: theme.spacing(1),
-        paddingBottom: theme.spacing(2),
-        color: '#585D78',
     },
     title: {
         flexGrow: 1,
-        alignSelf: 'flex-end',
-        color: 'white',
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
     },
-    button: {
-        color: 'white',
-    }
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+    list: {
+        width: 250,
+    },
+    fullList: {
+        width: 'auto',
+    },
 }));
 
 export function Header() {
-    const classes = myStyles();
+    const classes = useStyles();
+
     return (
         <div className={classes.root}>
             <AppBar position="static">
-                <Toolbar className={classes.toolbar}>
+                <Toolbar>
                     <IconButton
                         edge="start"
                         className={classes.menuButton}
                         color="inherit"
                         aria-label="open drawer"
                     >
-                        <MenuIcon/>
+                        <SideDrawer/>
                     </IconButton>
-                    <Typography className={classes.title} variant="h5" noWrap>
+                    <Typography className={classes.title} variant="h6" noWrap>
                         ArdaCraft
                     </Typography>
-                    <IconButton className={classes.button} aria-label="search">
-                        <SearchIcon/>
-                    </IconButton>
-                    <IconButton className={classes.button} aria-label="display more actions" edge="end">
-                        <MoreIcon/>
-                    </IconButton>
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon/>
+                        </div>
+                        <InputBase
+                            placeholder="Search…"
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                            inputProps={{'aria-label': 'search'}}
+                        />
+                    </div>
                 </Toolbar>
             </AppBar>
+        </div>
+    );
+}
+
+export function SideDrawer() {
+    const classes = useStyles();
+    const [state, setState] = React.useState({
+        left: false
+    });
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState({...state, [anchor]: open});
+    };
+
+    const list = (anchor) => (
+        <div
+            className={clsx(classes.list, {
+                [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+            })}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <List>
+                <ListItem button>
+                    <ListItemIcon></ListItemIcon>
+                    <ListItemText primary={"Home"}/>
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon></ListItemIcon>
+                    <ListItemText primary={"Forums"}/>
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon></ListItemIcon>
+                    <ListItemText primary={"Wiki"}/>
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon></ListItemIcon>
+                    <ListItemText primary={"Discord"}/>
+                </ListItem>
+            </List>
+            <Divider/>
+            <List>
+                <ListItem button>
+                    <ListItemIcon></ListItemIcon>
+                    <ListItemText primary={"Modpack"}/>
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon></ListItemIcon>
+                    <ListItemText primary={"Map"}/>
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon></ListItemIcon>
+                    <ListItemText primary={"About"}/>
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon></ListItemIcon>
+                    <ListItemText primary={"FAQ"}/>
+                </ListItem>
+            </List>
+        </div>
+    );
+
+    return (
+        <div>
+            {['left'].map((anchor) => (
+                <React.Fragment key={anchor}>
+                    <MenuIcon onClick={toggleDrawer(anchor, true)}/>
+                    <SwipeableDrawer
+                        anchor={anchor}
+                        open={state[anchor]}
+                        onClose={toggleDrawer(anchor, false)}
+                        onOpen={toggleDrawer(anchor, true)}
+                    >
+                        {list(anchor)}
+                    </SwipeableDrawer>
+                </React.Fragment>
+            ))}
         </div>
     );
 }
