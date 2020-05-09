9bbdc32760ce90f63da1d73c591d9a10bb30d22c
Michael Anderson <yadoneson175@gmail.com>
2020-05-09 16:14:18 -0600

    Build out homepage more

--- webapp/src/Pages/Home.js
+++ webapp/src/Pages/Home.js
@@ -1,36 +1,65 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+import {Link} from "react-router-dom";
 
 import '../App.css';
+import Grid from "@material-ui/core/Grid";
 
-export default class Home extends Component {
-    render() {
+export default function Home() {
 
-        return (
-            <div>
+    return (
+        <div>
+            <Images/>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="home-mtn">
+                <path fill="white"
+                      d="M0,0L205.7,64L411.4,96L617.1,224L822.9,256L1028.6,192L1234.3,160L1440,288L1440,320L1234.3,320L1028.6,320L822.9,320L617.1,320L411.4,320L205.7,320L0,320Z"/>
+            </svg>
+        </div>
+    );
+}
 
-                {this.images()}
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className="home-mtn">
-                    <path fill="white" d="M0,0L205.7,64L411.4,96L617.1,224L822.9,256L1028.6,192L1234.3,160L1440,288L1440,320L1234.3,320L1028.6,320L822.9,320L617.1,320L411.4,320L205.7,320L0,320Z"/>
-                </svg>
-            </div>
-                )
-    }
+export function Images() {
+    const styles = {
+        paperContainer: {
+            height: 1080,
+            backgroundImage: `url(${"https://i.imgur.com/BXphPfC.jpg"})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            margin: 'auto'
+        }
+    };
 
-    images() {
-        const styles = {
-            paperContainer: {
-                height: 1080,
-                backgroundImage: `url(${"https://i.imgur.com/BXphPfC.jpg"})`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center'
-            }
-        };
-
-        return (
+    return (
+        <div>
             <Paper style={styles.paperContainer}>
+                <div className={"home-text"}>
+                    <Text/>
+                </div>
             </Paper>
-        )
-    }
+        </div>
+    )
+}
+
+export function Text() {
+    return (
+        <div>
+            <Grid item xs={12}>
+                <Typography variant={"h4"} align={"center"} color={"textPrimary"}>
+                    Founded in 2014,
+                </Typography>
+                <Typography variant={"subtitle1"} align={"center"} color={"textPrimary"}>
+                    ArdaCraft seeks to recreate J.R.R. Tolkien's Middle-earth in Minecraft
+                </Typography>
+                <div className={"home-button"}>
+                    <Button variant="contained" size="medium" component={Link} to={"/modpack"}>
+                        Join now!
+                    </Button>
+                </div>
+            </Grid>
+        </div>
+    );
 
 }
