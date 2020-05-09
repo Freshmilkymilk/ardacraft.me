a5a662856611323a74dd5d4d2da15f0481a2acb1
Michael Anderson <yadoneson175@gmail.com>
2020-05-09 13:51:47 -0600

    Finish map

--- webapp/src/Pages/Map.js
+++ webapp/src/Pages/Map.js
@@ -1,10 +1,21 @@
 import React, {Component} from "react";
 
+import '../App.css';
+import Button from "@material-ui/core/Button";
+
 export default class Map extends Component {
     render() {
 
         return (
             <div>
+                <div className="map">
+                    <div className="map-container">
+                        <iframe src="https://ardacraft.io/map"></iframe>
+                    </div>
+                </div>
+                <div className="map-button">
+                    <Button variant="contained" size="large" href="https://ardacraft.io/map" target="_blank">View Larger</Button>
+                </div>
             </div>
         );
     }
